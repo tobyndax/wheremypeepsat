951d860cf8e7bd8153ae9f7907636eb69a2f7a74
Jens Edhammer <tobyndax@gmail.com>
2020-09-13 14:13:25 +0200

    Added additional test data

--- tests/mainTest.py
+++ tests/mainTest.py
@@ -6,8 +6,7 @@ from WhereMyPeepsAt import *
 
 parser = createParser()
 args = parser.parse_args("""
-        testData/output.wav
-        --feedback
+        testData/0015.wav
         --start 45500
         --end 48950
     """.split())
@@ -16,11 +15,30 @@ assert(main(args) == 6)
 print(main(args))
 
 args = parser.parse_args("""
-testData/output.wav
---feedback
+testData/0015.wav
 --start 8700
 --end 28700
 """.split())
 numPeeps = main(args)
 print(numPeeps)
 assert(numPeeps == 38)
+
+
+args = parser.parse_args("""
+testData/0024.wav
+--start 6000
+--end 186000
+""".split())
+numPeeps = main(args)
+
+print(numPeeps)
+assert(numPeeps == 277)
+
+args = parser.parse_args("""
+testData/0015.wav
+--start 8000
+--end 188000
+""".split())
+numPeeps = main(args)
+print(numPeeps)
+assert(numPeeps == 270)
